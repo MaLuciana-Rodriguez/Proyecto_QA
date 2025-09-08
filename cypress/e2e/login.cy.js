775516af685a2dd6b9792ddf841c5d3e8d099a2a
MaLuciana-Rodriguez <lrodriguez@portinos.com>
2025-09-08 15:35:06 -0300

    Login/logout caso de prueba

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,15 @@
+const loginEmailAdress = '#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=email]:nth-child(2)'
+const Password = '#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=password]:nth-child(3)'
+const loginButton = '#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > button'
+
+describe('Login/logout caso de prueba'), () => {
+  it('should log in successfully with valid credentials'), () => {
+    cy.visit('https://automationexercise.com/login') // Replace with the actual login URL}
+    cy.get(loginEmailAdress).type('lrodriguez@portinos.com') // Replace with the actual email input selector
+    cy.get(Password).type('Luportinos') // Replace with the actual password input selector
+    cy.get(loginButton).click() // Replace with the actual login button selector
+  }
+}
+
+
+
