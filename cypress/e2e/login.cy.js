6e7276db3991104154664a2731333fc16d5b0554
MaLuciana-Rodriguez <lrodriguez@portinos.com>
2025-09-17 11:17:35 -0300

    signup button

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,6 +2,7 @@ const loginEmailAdress = '#form > div > div > div.col-sm-4.col-sm-offset-1 > div
 const Password = '#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=password]:nth-child(3)'
 const loginButton = '#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > button'
 const Name = '#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=text]:nth-child(4)'
+const signupButton = '#header > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a'  
 
 
 describe('Login/logout caso de prueba'), () => {
@@ -10,8 +11,12 @@ describe('Login/logout caso de prueba'), () => {
     cy.get(loginEmailAdress).type('lrodriguez@portinos.com') // Replace with the actual email input selector
     cy.get(Password).type('Luportinos') // Replace with the actual password input selector
     cy.get(loginButton).click() // Replace with the actual login button selector
+describe ('Signup/login button'), () => {
+  it('should display signup/login button'), () => {
+    cy.visit('https://automationexercise.com/')
     cy.get(Name).should('contain', 'Luciana') // Replace with the actual selector that contains the user's name after login
     cy.get(loginEmailAdress).type('lrodriguez@portinos.com') // Replace with the actual email input selector
+    cy.get (signupButton).click() // Replace with the actual logout button selector 
 
   }
 }
